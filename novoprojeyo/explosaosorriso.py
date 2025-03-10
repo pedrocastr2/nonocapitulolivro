bdf1cb7b1c8186c874ee177855278357d37f4fcd
pedrocastr <102086416+pedrocastr2@users.noreply.github.com>
2025-03-10 19:46:02 -0300

    Add files via upload

---
+++ novoprojeyo/explosaosorriso.py
@@ -0,0 +1,68 @@
+import pygame 
+import random
+
+
+BLACK=(0,0,0)
+pygame.init()
+screen =pygame.display.set_mode([800,600]) 
+pygame.display.set_caption("Explosão sorriso")
+mousedown = False
+keep_going = True
+clock = pygame.time.Clock()
+pic = pygame.image.load("Crazysmile2.bmp")
+colorkey = pic.get_at((0,0))
+pic.set_colorkey(colorkey)
+sprite_list = pygame.sprite.Group()
+
+
+class Smiley(pygame.sprite.Sprite):
+    
+    def __init__(self,pos,xvel,yvel):
+    
+      
+         super().__init__() 
+         self.image = pic
+         self.scale = random.randint(10, 100)
+         self.image = pygame.transform.scale(self.image, (self.scale,self.scale))
+         self.rect = self.image.get_rect()
+         self.rect.x = pos[0] - self.scale/2
+         self.rect.y = pos[1] - self.scale/2
+         self.xvel = xvel 
+         self.yvel = yvel
+
+    def update(self):
+      self.rect.x += self.xvel
+      self.rect.y += self.yvel
+    
+      if self.rect.x <= 0 or self.rect.x> screen.get_width() - self.scale:
+         self.xvel = -self.xvel
+         
+      if self.rect.y <= 0 or self.rect.y> screen.get_height() - self.scale:    
+         self.yvel = -self.yvel
+
+while keep_going: #loop de jogo
+    for event in pygame.event.get():
+        
+       if event.type ==pygame.QUIT:
+         keep_going = False  
+         
+         
+       if event.type ==pygame.MOUSEBUTTONDOWN:
+          mousedown = True
+          
+       if event.type ==pygame.MOUSEBUTTONUP:   
+           mousedown = False
+           
+    screen.fill((BLACK)) #Limpa a tela
+    sprite_list.update()
+    sprite_list.draw(screen)
+    pygame.display.update()
+    clock.tick(60)  #Limite  
+    
+    
+    if mousedown:
+        speedx = random.randint(-5,5)
+        speedy = random.randint(-5,5)
+        newSmiley = Smiley(pygame.mouse.get_pos(), speedx, speedy)
+        sprite_list.add(newSmiley)
+pygame.quit()
